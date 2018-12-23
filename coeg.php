c799fa187b2bbc95ed641bb7e28a13cff6ee18ed
h3dw4r <38338029+h3dw4r@users.noreply.github.com>
2018-12-23 22:31:17 +0700

    Add files via upload

---
+++ coeg.php
@@ -0,0 +1,7 @@
+<?php
+$ids = '15256';
+//pake ssl capture http://saosdeveloper.club/mtc/loadbalance.php?id=[11668]
+//gabung di group telegram
+//https://t.me/joinchat/KYpSvQ_I4Hmx7TO_8-003Q
+//Youtube ==> Fariq Hunter 
+?>
